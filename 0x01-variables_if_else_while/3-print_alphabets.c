9a942e78d492d8b401253769afced5dbe92a125c
root <princessamphora@gmail.com>
2022-09-12 12:53:52 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - Prints the alaphabet in lowercase
  *
@@ -7,10 +8,12 @@
  */
 int main(void)
 {
+
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
+
 
 	putchar('\n');
 
