c08597faba3322795b21c3f2aa3345cc511da4f1
root <princessamphora@gmail.com>
2022-09-15 05:23:04 -0700

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,25 +12,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)	
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-
 
 	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-
-	
 	else
 	{
-
 		largest = c;
-	
 	}
-
 
 	return (largest);
 }
