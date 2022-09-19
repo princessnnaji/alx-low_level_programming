fcc592cb11eaeb99c7556ad843ea361d5f0a14aa
root <princessamphora@gmail.com>
2022-09-19 03:28:58 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 
 /**
- * reset_to_98 - takes pointer to an int and updates the value to 98
+ * reset_to_98 - update the int value to 98
  * @n: pointer given by the main function
  * description: change the value of the variable n to 98.
  *
@@ -11,7 +11,5 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	return(0);
 
 }
