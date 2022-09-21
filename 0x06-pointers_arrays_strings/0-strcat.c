3f77e67836c5bad23334a56f54c3006f903f4aa9
root <princessamphora@gmail.com>
2022-09-21 04:06:55 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int b = 0, c = 0;
+
+	while (dest[b] != '\0')
+		b++;
+
+	while (src[c] != '\0')
+	{
+	dest[b] = src[c];
+	b++;
+	c++;
+	}
+	dest[b] != '\0';
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+
+This file cntains the solution to the given tasks on the alx More pointers and arrays task"
