119ab79e9d1e5fe4db60f4b30d65cbca9464ba7d
root <princessamphora@gmail.com>
2022-09-21 06:44:05 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string by number of character
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i > n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
